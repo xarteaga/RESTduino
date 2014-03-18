eb05366215f0ada068d53a7748494633c883cc22
Xavier Arteaga <xavie.arteaga@gmail.com>
2014-03-18 22:26:35 +0100

    Added charts to Monitor

--- UserGUI/js/control.js
+++ UserGUI/js/control.js
@@ -3,7 +3,7 @@
  ******************************************************/
 function configDevice(id, type) {
     var e = document.getElementById(id);
-    switch(type){
+    switch (type) {
         case 'e':
             c = 'btn-default';
             t = 'Void';
@@ -105,9 +105,9 @@ function validateIPv4(addr) {
     if (res.length != 4) {
         return false;
     }
-    res.forEach( function(val){
+    res.forEach(function (val) {
         val = parseInt(val);
-        if (val >256 || val < 0)
+        if (val > 256 || val < 0)
             return false;
     });
     return true;
@@ -118,32 +118,32 @@ function validateIPv4(addr) {
  ******************************************************/
 /* Create Cross domain petition */
 function createCORSRequest(method, url) {
-	var xhr = new XMLHttpRequest();
-	if ("withCredentials" in xhr) {
-		// Check if the XMLHttpRequest object has a "withCredentials" property.
-		// "withCredentials" only exists on XMLHTTPRequest2 objects.
-		xhr.open(method, url, true);
-	} else if (typeof XDomainRequest != "undefined") {
-		// Otherwise, check if XDomainRequest.
-		// XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-		xhr = new XDomainRequest();
-		xhr.open(method, url);
-	} else {
-		// Otherwise, CORS is not supported by the browser.
-		xhr = null;
-	}
-	xhr.timeout = 1000;
-	xhr.onerror = function(e) {
-		stopAutoRead();
-		window.alert("The IP address does not point to a valid Server\nURL:'"+url+"'\n" + e);
-	};
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+        // Check if the XMLHttpRequest object has a "withCredentials" property.
+        // "withCredentials" only exists on XMLHTTPRequest2 objects.
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
+        // Otherwise, check if XDomainRequest.
+        // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        // Otherwise, CORS is not supported by the browser.
+        xhr = null;
+    }
+    xhr.timeout = 1000;
+    xhr.onerror = function (e) {
+        stopAutoRead();
+        window.alert("The IP address does not point to a valid Server\nURL:'" + url + "'\n" + e);
+    };
 
-	xhr.ontimeout = function() {
-		stopAutoRead();
-		window.alert("The IP address is unreachable.\nURL:'"+url+"'");
-	};
+    xhr.ontimeout = function () {
+        stopAutoRead();
+        window.alert("The IP address is unreachable.\nURL:'" + url + "'");
+    };
 
-  return xhr;
+    return xhr;
 }
 
 /******************************************************
@@ -156,7 +156,7 @@ function configDevProtected(path) {
     }
 
     // Create request and check if the navigation browser is valid
-    $.get(path).done(function(data){
+    $.get(path).done(function (data) {
         showConfig(data);
     });
 }
@@ -231,9 +231,9 @@ function stopAutoRead() {
     document.getElementById("startStopButtons").innerHTML = "<button type='button' class='btn btn-success' onclick='startAutoRead()'>Start</button><button type='button' class='btn btn-default'>Stop</button>";
 }
 
-function errorReading (){
+function errorReading() {
     failedReads += 1;
-    if (failedReads == 3){
+    if (failedReads == 3) {
         failedReads = 0;
         stopAutoRead();
         window.alert('Three failed trials to read sensors. Did you write properly the ip address?');
@@ -258,22 +258,26 @@ function readValues() {
         return;
     }
 
-	// Create request and check if the navigation browser is valid
-	var xhr = createCORSRequest('GET', url);
-	if (!xhr) {
-		stopAutoRead();
-		window.alert('CORS not supported');
-		return;
-	}
-	// Response handlers
-	xhr.onload = function() {
-		showPorts (xhr.responseText);
-	};
-	xhr.timeout = 2000;
-	xhr.onError = function(e){errorReading();};
-	xhr.onTimeout = function(){errorReading();};
+    // Create request and check if the navigation browser is valid
+    var xhr = createCORSRequest('GET', url);
+    if (!xhr) {
+        stopAutoRead();
+        window.alert('CORS not supported');
+        return;
+    }
+    // Response handlers
+    xhr.onload = function () {
+        showPorts(xhr.responseText);
+    };
+    xhr.timeout = 2000;
+    xhr.onError = function (e) {
+        errorReading();
+    };
+    xhr.onTimeout = function () {
+        errorReading();
+    };
 
-	xhr.send();
+    xhr.send();
 }
 
 function setOutput(code) {
@@ -306,8 +310,8 @@ function nextDiscover() {
     // Generate Ping
     new ping(discoveryIpCurrent, function (ip, status, e) {
         var element = document.getElementById("progressbar");
-        percent =  parseFloat(element.getAttribute("percent")) + 1 / (discoveryCount) * 100.0;
-        element.setAttribute("percent",  percent);
+        percent = parseFloat(element.getAttribute("percent")) + 1 / (discoveryCount) * 100.0;
+        element.setAttribute("percent", percent);
         element.setAttribute("style", "width: " + percent + "%");
         document.getElementById("progresslbl").innerHTML = Math.round(percent) + "% Complete";
 
@@ -323,7 +327,7 @@ function nextDiscover() {
         }
 
         if (label != 'undefined') {
-            document.getElementById("devicesTable").innerHTML += "<tr><td>" + ip + "</td><td><center>" + label + "</center></td></tr>";
+            document.getElementById("devicesTable").innerHTML += "<tr><td>" + ip + "</td><td>" + label + "</td></tr>";
         }
     });
 
@@ -351,7 +355,7 @@ function startDiscovery() {
 
     // Check if the IP addresses order
     discoveryCount = countAddr(stopAddrInt, startAddrInt);
-    if (discoveryCount < 0){
+    if (discoveryCount < 0) {
         window.alert("The start address must be lower than end address (count: " + discoveryCount + ")");
         return;
     }
@@ -366,8 +370,8 @@ function startDiscovery() {
     discoveryIndex = 0;
 
     document.getElementById("scanCtrl").innerHTML =
-                    "<button type='button' class='btn btn-default' onclick='startDiscovery()'> Start </button>" +
-                    "<button type='button' class='btn btn-danger' onclick='stopDiscovery()'> Stop </button>";
+        "<button type='button' class='btn btn-default' onclick='startDiscovery()'> Start </button>" +
+            "<button type='button' class='btn btn-danger' onclick='stopDiscovery()'> Stop </button>";
     discoveryContinue = true;
     nextDiscover(); // First discovery
 }
@@ -421,36 +425,106 @@ function ping(ip, callback) {
     }
 }
 
+
+function raw2celsius(raw) {
+    rb = 10000; // Resistance 1
+    res = 1013; // ADC Resolution
+    beta = 3950.0;
+    ginf = 120.15;
+    kelvin = 273.15;
+    rth = rb * (res / raw - 1);
+
+    temp = beta / Math.log(rth * ginf);
+    return (temp - kelvin).toFixed(1);
+}
+
+function raw2percent(raw) {
+    min = 10;
+    max = 895;
+
+    if (raw < min)
+        return 0.0;
+    else if (raw > max)
+        return 100.0;
+
+    return (100 * (raw - min) / (max - min)).toFixed(1);
+}
+
+function raw2light(raw) {
+    min = 170;
+    max = 761;
+
+    if (raw < min)
+        return 0.0;
+    else if (raw > max)
+        return 100.0;
+
+    return (100 * (raw - min) / (max - min)).toFixed(1);
+}
+
 /******************************************************
  *                  Update PORT states                *
  ******************************************************/
 function showPorts(txtPort) {
-    if (txtPort!="object")
+    if (txtPort != "object")
         values = JSON.parse(txtPort);
     else
         values = txtPort;
 
-    table = document.getElementById("analogReads");
+    var tableContent = "",
+        modals = "";
+
     document.getElementById("devName2").innerHTML = values.deviceName.replace("_", " ").replace("%20", " ");
-    table.innerHTML = "";
     for (i = 0; i < 6; i++) {
         port = "A" + i;
+        type = values.inputs[i].type;
         value = values.inputs[i].val;
-        table.innerHTML += "<tr><td><center>" + port + "</center></td><td><center>" + value + "</center></td></tr>";
+        switch (type) {
+            case "t": // Temperature
+                value = raw2celsius(value) + " ºC";
+                break;
+            case "p": // Potentiometer
+                value = raw2percent(value) + " %";
+                break;
+            case "l": // Light
+                value = raw2light(value) + " % of light";
+                break;
+            case "r": // Raw
+                value = value;
+                break;
+            case "b": // Binary (Switch/Button)
+                if (value > 512)
+                    value = "On";
+                else
+                    value = "Off";
+                break;
+            default: // Unknown or empty/void
+                value = "Empty"
+                break;
+        }
+        tableContent += "<tr><td style='text-align:center;'>" + port + "</td><td style='text-align:center;'>" + value + "</td>";
+        if (modalsEnabled()) {
+            tableContent += "<td>" + generateModalButton(i, type) + "</td>";
+            modals += generateModalWindow(i);
+        }
+        tableContent += "</tr>";
     }
-    table = document.getElementById("actuators");
-    table.innerHTML = "";
+    document.getElementById("analogReads").innerHTML = tableContent;
+    document.getElementById("modals").innerHTML = modals;
+
+    tableContent = "";
     for (i = 0; i < 6; i++) {
         port = "O" + i;
         value = values.outputs[i].val;
         if (value == "On") {
-            table.innerHTML += "<tr><td><center>" + port + "</center></td><td><center>" + value + "</center></td><td><div class='btn-group btn-group-sm'><button class='lbl btn-success' onclick='setOutput(\"" + i + "1\")'>On</button><button class='lbl btn-danger btn-xs' onclick='setOutput(\"" + i + "0\")'>Off</button></div></td></tr>";
+            tableContent += "<tr><td style='text-align:center;'>" + port + "</td><td style='text-align:center;'>" + value + "</td><td><div class='btn-group btn-group-sm'><button class='lbl btn-success' onclick='setOutput(\"" + i + "1\")'>On</button><button class='lbl btn-danger btn-xs' onclick='setOutput(\"" + i + "0\")'>Off</button></div></td></tr>";
         } else if (value == "Off") {
-            table.innerHTML += "<tr><td><center>" + port + "</center></td><td><center>" + value + "</center></td><td><div class='btn-group btn-group-sm'><button class='lbl btn-success' onclick='setOutput(\"" + i + "1\")'>On</button><button class='lbl btn-danger btn-xs' onclick='setOutput(\"" + i + "0\")'>Off</button></div></td></tr>";
+            tableContent += "<tr><td style='text-align:center;'>" + port + "</td><td style='text-align:center;'>" + value + "</td><td><div class='btn-group btn-group-sm'><button class='lbl btn-success' onclick='setOutput(\"" + i + "1\")'>On</button><button class='lbl btn-danger btn-xs' onclick='setOutput(\"" + i + "0\")'>Off</button></div></td></tr>";
         } else {
-            table.innerHTML += "<tr><td><center>" + port + "</center></td><td><center>" + value + "</center></td><td></td></tr>";
+            tableContent += "<tr><td style='text-align:center;'>" + port + "</td><td style='text-align:center;'>" + value + "</td><td></td></tr>";
         }
     }
+    document.getElementById("actuators").innerHTML = tableContent;
 }
 
 /******************************************************
@@ -515,4 +589,157 @@ if (arduinoIp != "") {
     document.getElementById("startIP").value = "192.168.10.1";
     document.getElementById("endIP").value = "192.168.10.10";
     configDev("config");
+}
+
+function modalsEnabled() {
+    if (typeof d3 !== 'undefined' && $(window).width() > 800)
+        return true;
+
+    return false;
+}
+
+function generateModalButton(identifier, type) {
+    //return "<button class='btn btn-primary btn-sm' href='#" + identifier + "' data-toggle='modal'>Graph</button>";
+    return "    <a class='btn btn-mini' data-toggle='modal' onclick='plotGraph(" + identifier + ", \"" + type + "\")' href='#modal" + identifier + "' class='btn btn-primary btn-large'>Graph</a>";
+}
+
+function generateModalWindow(identifier) {
+    var modal = "";
+    modal += ("<div class='modal hide fade' id='modal" + identifier + "' tabindex='-1' role='dialog' aria-labelledby='modal" + identifier + "Label' aria-hidden='true'>");
+    modal += ("<div class='modal-header'>");
+    modal += "<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>";
+    modal += "<h4 class='modal-title' id='modal" + identifier + "Label'>" + "TITLE" + "</h4>";
+    modal += ("</div>");
+    modal += ("<div class='modal-body'>");
+    modal += ("<div id='loadinggif" + i + "' style='text-align:center;'><img src='https://github.com/xarteaga/RESTduino/blob/864de606cb76de2b70070e79729b9716ef1cb570/UserGUI/img/loading.gif?raw=true'/></div>");
+    modal += ("<div id='graph" + i + "' style='width:530px; height:0px;'></div>");
+    modal += ("</div>");
+    modal += ("<div class='modal-footer'>");
+    modal += ("<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>");
+    modal += ("</div>");
+    modal += ("</div>");
+    modal += ("</div>");
+    return modal;
+}
+
+function plotGraph(input, type) {
+    // Stop automatic read
+    stopAutoRead();
+
+    // Get IP address and port if any
+    res = document.getElementById("ipAddress2").value.split(":");
+    if (res.length != 2)
+        res[1] = 80;
+    if (document.domain != res[0])
+        url = "http://" + res[0] + ":" + res[1] + "/" + "hist" + input;
+    else
+        url = "/" + "hist" + input;
+
+    // Get title, min, max and conversion function depending on the type of sensor.
+    var ytitle, mimimum, maximum, conversion;
+
+    switch (type) {
+        case "t": /* Temperature */
+            ytitle = "Temperature [ºC]"
+            maximum = 50;
+            minimum = 10;
+            conversion = raw2celsius;
+            break;
+        case "l": /* Light */
+            ytitle = "Light percent [%]";
+            maximum = 100;
+            minimum = 0;
+            conversion = raw2light;
+            break;
+        case "p": /* Potentiometer */
+            ytitle = "Potentiometer percent [%]";
+            maximum = 100;
+            minimum = 0;
+            conversion = raw2percent;
+            break;
+        default:
+            ytitle = "Raw data [10 bit scale]";
+            maximum = 1023;
+            minimum = 0;
+            conversion = function (value) {
+                return value;
+            };
+            break;
+    }
+
+    var graphdiv = document.getElementById("graph" + input);
+    graphdiv.innerHTML = "";
+    graphdiv.style.height = "400px";
+    document.getElementById("loadinggif" + input).innerHTML = "<img src='https://github.com/xarteaga/RESTduino/blob/864de606cb76de2b70070e79729b9716ef1cb570/UserGUI/img/loading.gif?raw=true'/>"
+    document.getElementById("modal" + input + "Label").innerHTML = "Analog input " + input;
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = 650 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    // Data pattern example: Thu, 06 Mar 2014 09:13:21 GMT
+    var parseDate = d3.time.format("%a, %d %b %Y %H:%M:%S GMT").parse;
+
+    // Define axis
+    var x = d3.time.scale()
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var line = d3.svg.line()
+        .x(function (d) {
+            return x(d.date);
+        })
+        .y(function (d) {
+            return y(d.value);
+        });
+
+    var svg = d3.select("#graph" + input).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.json(url, function (data) {
+        document.getElementById("loadinggif" + input).innerHTML = "";
+        data.splice(0, 1);
+        data.forEach(function (d, index) {
+            d.date = parseDate(d.date);
+            d.value = conversion(+d.value);
+        });
+
+        x.domain(d3.extent(data, function (d) {
+            return d.date;
+        }));
+        y.domain([minimum, maximum]);
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text(ytitle);
+
+        svg.append("path")
+            .datum(data)
+            .attr("class", "line")
+            .attr("d", line);
+    });
 }
